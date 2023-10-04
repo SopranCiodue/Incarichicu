3c32b90d9cb5560551ac53381a0d3f9cf5e7c4ad
Vale-riad <valentinariad91@gmail.com>
2023-10-04 09:15:52 +0200

    Aggiunge url produzione

--- IncarichiCUFront/src/app/environment/environment.ts
+++ IncarichiCUFront/src/app/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
 production: false,
-urlService: "http://localhost:5000/"
+// urlService: "http://localhost:5000/"
+urlService: "https://incarichicu.sopranciodue.it:444/"
 }
